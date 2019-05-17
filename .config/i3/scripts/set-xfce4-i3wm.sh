3e68c2963a39bd08eab914ab5b23513f3a04dbf9
Pavel Safronov <pv.safronov@gmail.com>
2019-05-17 19:36:58 +0300

    Added xfce configs for i3

---
+++ .config/i3/scripts/set-xfce4-i3wm.sh
@@ -0,0 +1,7 @@
+xfconf-query -c xfce4-session -p /sessions/Failsafe/Client0_Command -t string -sa i3
+xfconf-query -c xfce4-session -p /sessions/Failsafe/Client1_Command -t string -sa ''
+xfconf-query -c xfce4-session -p /sessions/Failsafe/Client2_Command -t string -sa ''
+xfconf-query -c xfce4-session -p /sessions/Failsafe/Client3_Command -t string -sa ''
+xfconf-query -c xfce4-session -p /sessions/Failsafe/Client4_Command -t string -sa ''
+xfconf-query -c xfce4-session -p /sessions/Failsafe/Count -t int -s 1
+
---
+++ .config/i3/scripts/unset-xfce4-i3wm.sh
@@ -0,0 +1,6 @@
+xfconf-query -c xfce4-session -p /sessions/Failsafe/Client0_Command -r
+xfconf-query -c xfce4-session -p /sessions/Failsafe/Client1_Command -r
+xfconf-query -c xfce4-session -p /sessions/Failsafe/Client2_Command -r
+xfconf-query -c xfce4-session -p /sessions/Failsafe/Client3_Command -r
+xfconf-query -c xfce4-session -p /sessions/Failsafe/Client4_Command -r
+xfconf-query -c xfce4-session -p /sessions/Failsafe/Count -r
