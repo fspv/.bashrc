2a904e5dcf364645ea191f132df935898709bffc
Pavel Safronov <pv.safronov@gmail.com>
2019-09-23 09:38:09 +0300

    Window language is now stored on LRU cache

--- .config/i3/scripts/window-binded-layout.py
+++ .config/i3/scripts/window-binded-layout.py
@@ -5,13 +5,33 @@ import logging
 import subprocess
 import time
 import i3ipc
+from collections import OrderedDict
 
 log = logging.getLogger()
 log_handler = logging.StreamHandler(sys.stdout)
 log.addHandler(log_handler)
 log.setLevel(logging.DEBUG)
 
-windows = {}
+
+class LRU(OrderedDict):
+    'Limit size, evicting the least recently looked-up key when full'
+
+    def __init__(self, maxsize=128, *args, **kwds):
+        self.maxsize = maxsize
+        super().__init__(*args, **kwds)
+
+    def __getitem__(self, key):
+        value = super().__getitem__(key)
+        self.move_to_end(key)
+        return value
+
+    def __setitem__(self, key, value):
+        super().__setitem__(key, value)
+        if len(self) > self.maxsize:
+            oldest = next(iter(self))
+            del self[oldest]
+
+windows = LRU(1024)
 prev_window_id = 0
 
 
