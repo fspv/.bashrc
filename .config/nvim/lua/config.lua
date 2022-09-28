160eaf7c26c9161a18b4451a65e765ff5c5f1778
Pavel Safronov <pv.safronov@gmail.com>
2022-09-28 09:19:51 +0100

    More treesitter config

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -30,4 +30,6 @@ require'nvim-treesitter.configs'.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
+  incremental_selection = { enable = true },
+  textobjects = { enable = true },
 }
