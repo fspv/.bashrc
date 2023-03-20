b698acedfaa9eb0111674750544f9c79f0409d98
Pavel Safronov <pv.safronov@gmail.com>
2023-03-20 20:50:22 +0000

    Revive i3 config for nvidia pc

--- .config/i3/scripts/set-xfce4-i3wm.sh
+++ .config/i3/scripts/set-xfce4-i3wm.sh
@@ -1,4 +1,4 @@
-xfconf-query -c xfce4-session -p /sessions/Failsafe/Client0_Command -t string -sa i3
+xfconf-query -c xfce4-session -p /sessions/Failsafe/Client0_Command -t string -sa 'i3'
 xfconf-query -c xfce4-session -p /sessions/Failsafe/Client1_Command -t string -sa ''
 xfconf-query -c xfce4-session -p /sessions/Failsafe/Client2_Command -t string -sa ''
 xfconf-query -c xfce4-session -p /sessions/Failsafe/Client3_Command -t string -sa ''
