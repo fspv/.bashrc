daf2c23cfd4e5f1cc3b4f9a4096e0800aa53a8b6
Pavel Safronov <pv.safronov@gmail.com>
2023-04-26 23:05:26 +0100

    Fix open/closed lid detection on laptop

--- .config/sway/scripts/laptop-display-state-on-reload.sh
+++ .config/sway/scripts/laptop-display-state-on-reload.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-if grep -q open /proc/acpi/button/lid/LID0/state; then
+if grep -q open /proc/acpi/button/lid/LID*/state; then
     swaymsg output eDP-1 enable
 else
     swaymsg output eDP-1 disable
