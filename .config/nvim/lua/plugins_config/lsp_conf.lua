d37bff4aae369dfb29094ec045037ca337040908
Pavel Safronov <pv.safronov@gmail.com>
2023-05-05 16:20:40 +0100

    More convenient go to definition using telescope

--- .config/nvim/lua/plugins_config/lsp_conf.lua
+++ .config/nvim/lua/plugins_config/lsp_conf.lua
@@ -26,14 +26,12 @@ lsp.on_attach(function(_, bufnr)
     end)
 
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
     vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
     vim.keymap.set('n', '<space>wl', function()
         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, opts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', '<space>f', function()
@@ -47,6 +45,8 @@ lsp.on_attach(function(_, bufnr)
     vim.keymap.set("n", "gf", find_symbols, opts)
     vim.keymap.set('n', 'gr', require("telescope.builtin").lsp_references, opts)
     vim.keymap.set('n', 'gi', require("telescope.builtin").lsp_implementations, opts)
+    vim.keymap.set('n', 'gd', require("telescope.builtin").lsp_definitions, opts)
+    vim.keymap.set('n', '<space>D', require("telescope.builtin").lsp_type_definitions, opts)
 
 
     -- Definition highlight on cover
