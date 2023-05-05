759b1bcb2215561d2c6b35a5dcd7542e03e69deb
Pavel Safronov <pv.safronov@gmail.com>
2023-05-05 14:30:03 +0100

    Diable fancy barbar stuff

--- .config/nvim/lua/plugins_config/barbar_conf.lua
+++ .config/nvim/lua/plugins_config/barbar_conf.lua
@@ -3,32 +3,32 @@ local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap('n', 'gT', '<Cmd>BufferPrevious<CR>', opts)
 vim.api.nvim_set_keymap('n', 'gt', '<Cmd>BufferNext<CR>', opts)
 
-vim.api.nvim_create_autocmd('FileType', {
-    callback = function(tbl)
-        local set_offset = require('barbar.api').set_offset
-
-        local bufwinid
-        local last_width
-        local autocmd = vim.api.nvim_create_autocmd('WinScrolled', {
-            callback = function()
-                bufwinid = bufwinid or vim.fn.bufwinid(tbl.buf)
-
-                local width = vim.api.nvim_win_get_width(bufwinid)
-                if width ~= last_width then
-                    set_offset(width, 'FileTree')
-                    last_width = width
-                end
-            end,
-        })
-
-        vim.api.nvim_create_autocmd('BufWipeout', {
-            buffer = tbl.buf,
-            callback = function()
-                vim.api.nvim_del_autocmd(autocmd)
-                set_offset(0)
-            end,
-            once = true,
-        })
-    end,
-    pattern = 'NvimTree', -- or any other filetree's `ft`
-})
+-- vim.api.nvim_create_autocmd('FileType', {
+--     callback = function(tbl)
+--         local set_offset = require('barbar.api').set_offset
+--
+--         local bufwinid
+--         local last_width
+--         local autocmd = vim.api.nvim_create_autocmd('WinScrolled', {
+--             callback = function()
+--                 bufwinid = bufwinid or vim.fn.bufwinid(tbl.buf)
+--
+--                 local width = vim.api.nvim_win_get_width(bufwinid)
+--                 if width ~= last_width then
+--                     set_offset(width, 'FileTree')
+--                     last_width = width
+--                 end
+--             end,
+--         })
+--
+--         vim.api.nvim_create_autocmd('BufWipeout', {
+--             buffer = tbl.buf,
+--             callback = function()
+--                 vim.api.nvim_del_autocmd(autocmd)
+--                 set_offset(0)
+--             end,
+--             once = true,
+--         })
+--     end,
+--     pattern = 'NvimTree', -- or any other filetree's `ft`
+-- })
