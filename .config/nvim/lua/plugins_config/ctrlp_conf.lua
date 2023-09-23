3fa4eb23caba323117e5cd1181ff19c330abff4f
Pavel Safronov <pv.safronov@gmail.com>
2023-09-23 10:41:17 +0100

    Improved ctrlp config

--- .config/nvim/lua/plugins_config/ctrlp_conf.lua
+++ .config/nvim/lua/plugins_config/ctrlp_conf.lua
@@ -1,1 +1,7 @@
-vim.g.ctrlp_cmd = 'CtrlPBuffer'
+vim.g.ctrlp_cmd = "CtrlPBuffer"
+vim.g.ctrlp_mruf_max = 25000
+vim.g.ctrlp_match_window = "bottom,order:btt,min:1,max:30,results:30"
+vim.g.ctrlp_tilde_homedir = 1
+vim.g.ctrlp_extensions = {
+  "changes", "quickfix", "undo", "line",
+}
