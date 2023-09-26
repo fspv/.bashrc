770651f119bf780585526e71c775e0c363563202
Pavel Safronov <pv.safronov@gmail.com>
2023-09-26 13:58:50 +0100

    Fix ctrlp status line

--- .config/nvim/lua/plugins_config/lualine_conf.lua
+++ .config/nvim/lua/plugins_config/lualine_conf.lua
@@ -1,1 +1,18 @@
-require("lualine").setup()
+require("lualine").setup(
+  {
+    options = {
+      disabled_filetypes = {
+        statusline = { "ctrlp", "neo-tree" },
+      },
+    },
+    sections = {
+      lualine_a = {
+        {
+          'filename',
+          file_status = true,
+          path = 3
+        }
+      }
+    }
+  }
+)
