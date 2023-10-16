f24d106a6086da912ba5db4f56d9b1dc79294dfa
Pavel Safronov <pv.safronov@gmail.com>
2023-10-16 19:11:04 +0100

    More fancy lualine stuff

--- .config/nvim/lua/plugins_config/lualine_conf.lua
+++ .config/nvim/lua/plugins_config/lualine_conf.lua
@@ -5,12 +5,40 @@ require("lualine").setup(
         statusline = { "ctrlp", "neo-tree" },
       },
     },
+    tabline = {},
+    winbar = {},
     sections = {
       lualine_a = {
         {
           'filename',
           file_status = true,
-          path = 3
+          path = 3,
+          on_click = function()
+            vim.cmd("Neotree toggle")
+          end,
+        },
+        {
+          "lsp_progress",
+        },
+      },
+      lualine_b = {
+        {
+          "branch",
+          on_click = function()
+            vim.cmd("G")
+          end,
+        },
+        {
+          "diff",
+          on_click = function()
+            vim.cmd("Gdiffsplit")
+          end,
+        },
+        {
+          "diagnostics",
+          on_click = function()
+            vim.cmd("TroubleToggle")
+          end,
         }
       }
     }
