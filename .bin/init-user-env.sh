728d568367594f064a8a9faf211ae4bb1dacf722
Pavel Safronov <pv.safronov@gmail.com>
2024-04-07 21:57:14 +0100

    Move some stuff to ansible

--- .bin/init-user-env.sh
+++ .bin/init-user-env.sh
@@ -2,8 +2,6 @@
 
 set -uex
 
-mkdir -p ${HOME}/.local/bin
-
 test -d "${HOME}/.local/share/oh-my-zsh" || KEEP_ZSHRC=yes CHSH=no RUNZSH=no ZSH="${HOME}/.local/share/oh-my-zsh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 ZSH_CUSTOM="${HOME}/.local/share/oh-my-zsh/custom"
@@ -43,38 +41,7 @@ flatpak install -y --user flathub org.telegram.desktop
 flatpak override --user org.telegram.desktop --filesystem=${HOME}/Pictures
 flatpak override --user org.telegram.desktop --filesystem=${HOME}/Downloads
 
-mkdir -p ~/venv
-virtualenv -p python3 ~/venv/neovim
-
-set +x
-. ~/venv/neovim/bin/activate
-set -x
-
-pip install neovim jedi
-
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-sudo snap install rustup --classic
-sudo snap install rust-analyzer --beta
-rustup default stable
-rustup update
-
-sudo apt-get install -y flake8 mypy pycodestyle python3-pyflakes black isort
-sudo apt-get install -y nodejs npm
-sudo apt-get install -y clangd cppcheck flawfinder astyle clang-format clang-tidy uncrustify clangd clang cmake
-sudo apt install python3.10-venv
-
-sudo apt-get install -y exuberant-ctags universal-ctags
-
-sudo apt-get install -y golang-go
 go install github.com/jstemmer/gotags@latest
-
-mkdir -p ~/.local/share/fonts/fonts/nerdfonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/JetBrainsMono.zip -O ~/.local/share/fonts/fonts/tmp.zip
-unzip -o ~/.local/share/fonts/fonts/tmp.zip -d ~/.local/share/fonts/fonts/nerdfonts
-rm -rf ~/.local/share/fonts/fonts/tmp.zip
-fc-cache -fv
 
 curl -L https://sourcegraph.com/.api/src-cli/src_linux_amd64 -o ~/.bin/src
 chmod +x ~/.bin/src
