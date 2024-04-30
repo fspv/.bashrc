42c37255448fae079f700290470edb96d023e78a
Pavel Safronov <pv.safronov@gmail.com>
2024-04-30 14:52:29 +0100

    disable not needed features

--- .config/nvim/lua/plugins_config/lspsaga_conf.lua
+++ .config/nvim/lua/plugins_config/lspsaga_conf.lua
@@ -3,7 +3,15 @@ require("lspsaga").setup(
     symbol_in_winbar = {
       enable = false,
     },
+    implement = {
+      enable = false,
+      sign = false,
+      virtual_text = false,
+    },
     lightbulb = {
+      enable = false,
+    },
+    beacon = {
       enable = false,
     },
     breadcrumbs = {
