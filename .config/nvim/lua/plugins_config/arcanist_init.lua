3d9f02ec3992781aeb5baeb4a285e4de7fa78d81
Pavel Safronov <pv.safronov@gmail.com>
2024-05-31 08:51:46 +0100

    refactor arc

--- .config/nvim/lua/plugins_config/arcanist_init.lua
+++ .config/nvim/lua/plugins_config/arcanist_init.lua
@@ -1,3 +1,12 @@
+---@param message string
+---@return string?
+local function format_message(message)
+  message = message or ''
+  message = vim.trim(message)
+  message = message:gsub('\t', string.rep(' ', 4))
+  return message
+end
+
 -- Add an automatic linter for arcanist https://github.com/phacility/arcanist
 vim.api.nvim_create_autocmd(
   'BufWritePost',
@@ -12,23 +21,55 @@ vim.api.nvim_create_autocmd(
       if vim.fn.executable('arc') < 1 then
         return
       end
-      local function on_event(_, data)
-        local msg = table.concat(data, '\n')
-        msg = vim.trim(msg)
-        msg = msg:gsub('\t', string.rep(' ', 4))
-        if msg ~= '' then
-          vim.notify('arc: ' .. msg, vim.log.levels.INFO)
+      local command = { 'arc', 'lint', '--severity', 'autofix', '--apply-patches', args.file }
+      vim.system(
+        command,
+        {},
+        function(out)
+          if out.code == 0 then
+            vim.schedule(
+              function()
+                vim.notify('command finished: ' .. table.concat(command, ' '), vim.log.levels.INFO)
+                if vim.api.nvim_buf_get_name(0):sub(- #args.file) == args.file then
+                  local buf = vim.bo[vim.api.nvim_win_get_buf(0)]
+                  if not buf.readonly and not buf.modified then
+                    vim.cmd("edit")
+                  end
+                end
+              end
+            )
+          else
+            vim.schedule(
+              function()
+                vim.notify('command failed: ' .. table.concat(command, ' '), vim.log.levels.ERROR)
+                if vim.api.nvim_buf_get_name(0):sub(- #args.file) == args.file then
+                  local buf = vim.bo[vim.api.nvim_win_get_buf(0)]
+                  if not buf.readonly and not buf.modified then
+                    vim.cmd("edit")
+                  end
+                end
+              end
+            )
+          end
+
+          local stdout = format_message(out.stdout)
+          if stdout ~= '' then
+            vim.schedule(
+              function()
+                vim.notify('arc: ' .. stdout, vim.log.levels.INFO)
+              end
+            )
+          end
+
+          local stderr = format_message(out.stderr)
+          if stderr ~= '' then
+            vim.schedule(
+              function()
+                vim.notify('arc: ' .. stderr, vim.log.levels.ERROR)
+              end
+            )
+          end
         end
-        vim.cmd("edit")
-      end
-      vim.fn.jobstart(
-        { 'arc', 'lint', '--severity', 'autofix', '--apply-patches', args.file },
-        {
-          on_stdout = on_event,
-          on_stderr = on_event,
-          stdout_buffered = true,
-          stderr_buffered = true,
-        }
       )
     end,
   }
