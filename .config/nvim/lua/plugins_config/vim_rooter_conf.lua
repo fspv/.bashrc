0bf2db2d3b9d8ed0ff6dcb1fb28958736d996f7e
Pavel Safronov <pv.safronov@gmail.com>
2024-06-04 13:34:29 +0100

    smarter vim rooter config

--- .config/nvim/lua/plugins_config/vim_rooter_conf.lua
+++ .config/nvim/lua/plugins_config/vim_rooter_conf.lua
@@ -1,1 +1,15 @@
+-- Doc: https://github.com/airblade/vim-rooter
 vim.g.rooter_manual_only = 0
+vim.g.rooter_patterns = {
+  '.git',
+  '_darcs',
+  '.hg',
+  '.bzr',
+  '.svn',
+  'Makefile',
+  'package.json',
+  -- To cd for files residing in /tmp
+  -- this may break something else, so worth revisiting later
+  '>tmp',
+  'go[1-9].*'
+}
