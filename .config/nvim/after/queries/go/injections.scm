72c67f0fe4e6d3c24235a65ec00cbd1e0b0e2e8e
Pavel Safronov <pv.safronov@gmail.com>
2024-06-20 14:15:19 +0100

    Highlight markdown in go comments

--- .config/nvim/after/queries/go/injections.scm
+++ .config/nvim/after/queries/go/injections.scm
@@ -3,3 +3,6 @@
   (#match? @injection.content ".*(^|\n)(from.*import|def )")
   (#offset! @injection.content 0 1 0 -1)
   (#set! injection.language "python"))
+
+((comment) @injection.content
+  (#set! injection.language "markdown"))
