655d6a856d2286173c7f841ba2837d633e76258d
Pavel Safronov <pv.safronov@gmail.com>
2024-07-05 09:46:49 +0100

    fix a bug when telescope result opens on the first line

--- .config/nvim/lua/plugins_config/telescope_conf.lua
+++ .config/nvim/lua/plugins_config/telescope_conf.lua
@@ -29,7 +29,20 @@ require("telescope").setup(
       history = {
         path = '~/.local/share/nvim/telescope_history.sqlite3',
         limit = 100,
-      }
+      },
+      -- open files in the first window that is an actual file.
+      -- use the current window if no other window is available.
+      get_selection_window = function()
+        local wins = vim.api.nvim_list_wins()
+        table.insert(wins, 1, vim.api.nvim_get_current_win())
+        for _, win in ipairs(wins) do
+          local buf = vim.api.nvim_win_get_buf(win)
+          if vim.bo[buf].buftype == "" then
+            return win
+          end
+        end
+        return 0
+      end,
     },
     pickers = {
       lsp_references = { fname_width = 120 },
