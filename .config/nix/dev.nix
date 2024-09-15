aab7780722179178fdeb6c5a4f99a1439ae23123
Pavel Safronov <pv.safronov@gmail.com>
2024-09-15 13:40:39 +0000

    stricter bwrap

--- .config/nix/dev.nix
+++ .config/nix/dev.nix
@@ -100,12 +100,57 @@ pkgs.mkShell {
     # TODO: automatically source MANPATH
     # TODO: automatically source bash and zsh completions
 
-    bwrap --dev-bind / / \
-        --ro-bind /nix /nix \
-        --tmpfs /tmp \
-        --tmpfs /home/$(whoami)/.cache \
-        --tmpfs /etc/ssh/ssh_config.d \
+    bwrap \
+        --die-with-parent \
+        --unshare-ipc \
+        --unshare-cgroup \
         --share-net \
+        --bind /home/$(whoami) /home/$(whoami) \
+        --ro-bind /bin /bin \
+        --ro-bind /sbin /sbin \
+        --ro-bind /lib /lib \
+        --ro-bind /lib64 /lib64 \
+        --ro-bind /usr /usr \
+        --ro-bind /opt /opt \
+        --ro-bind /snap /snap \
+        --ro-bind /var /var \
+        --ro-bind /nix /nix \
+        --dev /dev \
+        --proc /proc \
+        --tmpfs /tmp \
+        --tmpfs /run/user/$(id -u) \
+        --tmpfs /home/$(whoami)/.local \
+        --tmpfs /home/$(whoami)/.config \
+        --tmpfs /home/$(whoami)/.cache \
+        --tmpfs /home/$(whoami)/.ssh \
+        --tmpfs /etc/ssh/ssh_config.d \
+        --bind /etc/passwd /etc/passwd \
+        --bind /etc/group /etc/group \
+        --bind /etc/resolv.conf /etc/resolv.conf \
+        --bind /etc/hosts /etc/hosts \
+        --bind-try /home/$(whoami)/.config/environment.d /home/$(whoami)/.config/environment.d \
+        --bind-try /home/$(whoami)/.config/autostart /home/$(whoami)/.config/autostart \
+        --bind-try /home/$(whoami)/.config/flake8 /home/$(whoami)/.config/flake8 \
+        --bind-try /home/$(whoami)/.config/gtk-3.0 /home/$(whoami)/.config/gtk-3.0 \
+        --bind-try /home/$(whoami)/.config/i3 /home/$(whoami)/.config/i3 \
+        --bind-try /home/$(whoami)/.config/i3status /home/$(whoami)/.config/i3status \
+        --bind-try /home/$(whoami)/.config/nix /home/$(whoami)/.config/nix \
+        --bind-try /home/$(whoami)/.config/nvim /home/$(whoami)/.config/nvim \
+        --bind-try /home/$(whoami)/.config/systemd /home/$(whoami)/.config/systemd \
+        --bind-try /home/$(whoami)/.config/pulse /home/$(whoami)/.config/pulse \
+        --bind-try /home/$(whoami)/.config/pycodestyle /home/$(whoami)/.config/pycodestyle \
+        --bind-try /home/$(whoami)/.config/sway /home/$(whoami)/.config/sway \
+        --bind-try /home/$(whoami)/.config/swaylock /home/$(whoami)/.config/swaylock \
+        --bind-try /home/$(whoami)/.config/terminator /home/$(whoami)/.config/terminator \
+        --bind-try /home/$(whoami)/.config/tmux /home/$(whoami)/.config/tmux \
+        --bind-try /home/$(whoami)/.config/waybar /home/$(whoami)/.config/waybar \
+        --bind-try /home/$(whoami)/.local/bin /home/$(whoami)/.local/bin \
+        --bind-try /home/$(whoami)/.local/include /home/$(whoami)/.local/include \
+        --bind-try /home/$(whoami)/.local/lib /home/$(whoami)/.local/lib \
+        --bind-try /home/$(whoami)/.local/share/oh-my-zsh /home/$(whoami)/.local/share/oh-my-zsh \
+        --bind-try /home/$(whoami)/.local/share/bin/wayland-user /home/$(whoami)/.local/share/bin/wayland-user \
+        --bind-try /home/$(whoami)/.local/share/nvim /home/$(whoami)/.local/share/nvim \
+        --bind-try /home/$(whoami)/.local/state/nvim /home/$(whoami)/.local/state/nvim \
         -- zsh
     zsh
   '';
