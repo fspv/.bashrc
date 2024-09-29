2cf96ed8646f3eaae7dc20102c72b4d6a45d29e0
Pavel Safronov <pv.safronov@gmail.com>
2024-09-29 08:32:59 +0000

    correct remotes and pull for bootstrap

--- .local/bin/bootstrap.sh
+++ .local/bin/bootstrap.sh
@@ -4,6 +4,7 @@ set -uex
 
 if [ ! -d ".git" ]; then
     git init
-    git remote add https://github.com/fspv/.bashrc.git
-    git pull
+    git remote add http https://github.com/fspv/.bashrc.git
+    git remote add origin git@github.com:fspv/.bashrc.git
+    git pull http master
 fi
