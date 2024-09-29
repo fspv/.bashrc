0709627eaa0e8f19a2a478c58fc3c103dfe69d93
Pavel Safronov <pv.safronov@gmail.com>
2024-09-29 08:45:16 +0000

    ignore existing files on bootstrap

--- .local/share/bin/bootstrap.sh
+++ .local/share/bin/bootstrap.sh
@@ -6,5 +6,8 @@ if [ ! -d ".git" ]; then
     git init
     git remote add http https://github.com/fspv/.bashrc.git
     git remote add origin git@github.com:fspv/.bashrc.git
+    git fetch http
+    git checkout -f http/master
+    git checkout master
     git pull http master
 fi
