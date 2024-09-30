60cb02ccecfd160888a704dc02e417cdec628cc3
Pavel Safronov <pv.safronov@gmail.com>
2024-09-30 04:57:41 +0000

    added nix state dir to bwrap

--- .config/nix/dev.nix
+++ .config/nix/dev.nix
@@ -196,6 +196,7 @@ pkgs.mkShell {
         --bind-try $HOME/.local/share/bin $HOME/.local/share/bin \
         --bind-try $HOME/.local/share/nvim $HOME/.local/share/nvim \
         --bind-try $HOME/.local/state/nvim $HOME/.local/state/nvim \
+        --bind-try $HOME/.local/state/nix $HOME/.local/state/nix \
         -- zsh
     zsh
   '';
