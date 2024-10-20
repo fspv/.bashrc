6eb7f1cb8aed0c269042f0757d81f67cc473a925
Pavel Safronov <pv.safronov@gmail.com>
2024-10-20 12:32:46 +0100

    fix notification daemons race in sway

--- .config/sway/scripts/log_wrapper.sh
+++ .config/sway/scripts/log_wrapper.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -ue
 
 COMMAND=$(printf ' %q' "$@")
-_NAME=$(basename $1)
+_NAME=$(basename "$1")
 APP=${_NAME%.*}
 
 USER_TMP_DIR="/run/user/$(id -u)/sway_log"
-mkdir -p ${USER_TMP_DIR}
+mkdir -p "${USER_TMP_DIR}"
 
 LOG=${USER_TMP_DIR}/${APP}.log
 
 echo "[log_wrapper] Running ${COMMAND}, output log to ${LOG}"
 
-sh -c "${COMMAND}" >> ${LOG} 2>&1 || echo "[log_wrapper] App terminated ${COMMAND}, see log ${LOG}"
+sh -xc "${COMMAND}" >> "${LOG}" 2>&1 || echo "[log_wrapper] App terminated ${COMMAND}, see log ${LOG}"
