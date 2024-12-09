ef8759902ad28a244e181c465213726de14e2510
Pavel Safronov <pv.safronov@gmail.com>
2024-12-09 19:03:15 +0000

    add docker build for nix

---
+++ .config/nix/docker.nix
@@ -0,0 +1,12 @@
+# nix-build ~/.config/nix/docker.nix && ./result | docker load && docker run --mount type=bind,source="$(pwd)",target=/build -it nix-shell-dev:latest
+{
+  pkgs ? import (fetchTarball "https://github.com/nixos/nixpkgs/archive/nixos-24.05.tar.gz") {},
+}:
+let
+  shellDrv = import ./dev.nix { inherit pkgs; };
+in
+pkgs.dockerTools.streamNixShellImage {
+  name = "nix-shell-dev";
+  tag = "latest";
+  drv = shellDrv;
+}
