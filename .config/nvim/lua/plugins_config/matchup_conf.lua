6c365de1b461cb50294be094135190da7caaa156
Pavel Safronov <pv.safronov@gmail.com>
2025-07-09 12:37:36 +0000

    dont break selection copy to clipboard

--- .config/nvim/lua/plugins_config/matchup_conf.lua
+++ .config/nvim/lua/plugins_config/matchup_conf.lua
@@ -1,3 +1,4 @@
 vim.g.matchup_matchparen_deferred = 1
 vim.g.matchup_matchparen_hi_surround_always = 1
+vim.g.matchup_matchparen_nomode = "ivV\x16" -- the last one is Ctrl+V
 vim.g.matchup_matchparen_offscreen = { method = "popup" }
