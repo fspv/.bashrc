fb132e9b95a72322bf7bace11c01707c56be4f70
Pavel Safronov <pv.safronov@gmail.com>
2025-08-25 09:47:53 +0100

    fix logger

--- .config/sway/scripts/log_wrapper.sh
+++ .config/sway/scripts/log_wrapper.sh
@@ -14,7 +14,7 @@ echo "[log_wrapper] Running ${COMMAND}, output log to ${LOG}"
 if ! sh -xc "${COMMAND}" >> "${LOG}" 2>&1
 then
     echo "[log_wrapper] App terminated ${COMMAND}, see log ${LOG}"
-    if "$_NAME" != "waybar"
+    if [ "$_NAME" != "waybar" ]
     then
         notify-send "App terminated ${COMMAND}, see log ${LOG}"
     fi
