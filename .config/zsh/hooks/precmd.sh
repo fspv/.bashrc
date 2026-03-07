38fba91ad4cacda211672265038ecbd9dba86443
Pavel Safronov <pv.safronov@gmail.com>
2026-03-07 00:33:39 +0000

    fix zsh path

--- .config/zsh/hooks/precmd.sh
+++ .config/zsh/hooks/precmd.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
 # Sync pane context (path, git branch, k8s) into tmux pane-local variables
 # Receives $1 = current working directory from the precmd hook
 if [[ -n "$TMUX" ]]; then
